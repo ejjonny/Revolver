91d687a4616e00ea44dd2a23f8f9b2a2312f556a
Petr Mánek <petr.manek@seznam.cz>
2017-07-30 19:16:03 +0200

    Updated umbrella header.

--- Sources/Revolver.h
+++ Sources/Revolver.h
@@ -1,4 +1,4 @@
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Revolver.
 FOUNDATION_EXPORT double RevolverVersionNumber;
@@ -7,5 +7,3 @@ FOUNDATION_EXPORT double RevolverVersionNumber;
 FOUNDATION_EXPORT const unsigned char RevolverVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Revolver/PublicHeader.h>
-
-
