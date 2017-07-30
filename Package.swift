9fa2c356184a6d29ee816302d6a8f3073896e9c3
Petr Mánek <petr.manek@seznam.cz>
2017-07-30 18:45:13 +0200

    Removed Swift-AI dependency.

--- Package.swift
+++ Package.swift
@@ -6,9 +6,7 @@ let package = Package(
     name: "Revolver",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/Swift-AI/Swift-AI.git",
-                 majorVersion: 2),
         .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git",
-                 majorVersion: 3),
+                 majorVersion: 3)
     ]
 )
